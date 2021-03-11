49285f7790227708b3d36395db2346739125d8d0
Daniel Paredes <danieljparedes@gmail.com>
2021-03-10 21:33:18 -0500

    Update initService.java

--- src/gatorShare/initService.java
+++ src/gatorShare/initService.java
@@ -3,7 +3,7 @@ package gatorShare;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class initService {
+public class initService extends Thread {
 
     private Socket socket;
     private ServerSocket server = null;
@@ -11,7 +11,6 @@ public class initService {
     public initService(int port) {
         System.out.println("[TEST]: Starting server...");
         try {
-            socket = new Socket("192.168.0.1", 1121);
             server = new ServerSocket(port);
             System.out.println("[TEST]: Trying to establish connection to client.");
             socket = server.accept();
