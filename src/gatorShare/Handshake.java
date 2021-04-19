29b31120d488f266c93893b1f7e9166702b55a3e
siddharth199829 <79243535+siddharth199829@users.noreply.github.com>
2021-04-19 13:18:02 -0400

    Update Handshake.java

--- src/gatorShare/Handshake.java
+++ src/gatorShare/Handshake.java
@@ -8,12 +8,9 @@ public class Handshake {
 
     private int ID;
     private Socket socket = null;
-    private String header;
-
-    public Handshake(String message, int myID) {
-        System.out.println(message.substring(28));
-        System.out.println(myID);
-    }
+    private static final long serialVersionUID = -1482860868859618509L;
+	private static final String Header = "P2PFILESHARINGPROJECT";
+	private final String peerMsgHeader;
     
     public Handshake(String message, int myID, String header) {
     	this.ID = myID;
@@ -21,4 +18,59 @@ public class Handshake {
     	System.out.println(message.substring(28));
         System.out.println(myID);
     }
+    
+     /**
+	 * @return the ID
+	 */
+    public int getID() {
+		return ID;
+	}
+    
+    /**
+	 * @param ID
+	 *            the ID to set
+	 */
+	public void setID(int peerID) {
+		this.ID = ID;
+	}
+    
+    	/**
+	 * @return the peerMsgHeader
+	 */
+	public String getPeerMsgHeader() {
+		return peerMsgHeader;
+	}
+
+	/**
+	 * @return the header
+	 */
+	public static String getHeader() {
+		return Header;
+	}
+    
+    	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		return sb.append("[Header :").append(getHeader()).append("]\n").append("[ ID: ").append(this.ID).append("]")
+				.toString();
+	}
+
+	public void SendHandShake(OutputStream out) throws IOException {
+		ObjectOutputStream opStream = new ObjectOutputStream(out);
+		opStream.writeObject(this);
+		System.out.println("Sending handshake message to peer " + this.ID);
+	}
+
+	// return value could be changed to HandShakeMsg if header is also needed
+	public int ReceiveHandShake(InputStream in) throws IOException {
+		try {
+			ObjectInputStream ipStream = new ObjectInputStream(in);
+			HandShake Response = (HandShake) ipStream.readObject();
+			return Response != null ? Response.peerID : -1;
+		} catch (ClassNotFoundException e) {
+			System.out.println(e);
+		}
+		return -1;
+	}
 }
+    
